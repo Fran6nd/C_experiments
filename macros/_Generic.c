7e6ca0145f8309826b3ffdc36a8203c71539c61e
François NOUAILLE DEGORCE <francois.nouaille-degorce@outscale.com>
2020-12-05 14:55:02 +0100

    wip

--- macros/_Generic.c
+++ macros/_Generic.c
@@ -1,10 +1,21 @@
 #!/usr/local/bin/tcc -run
 #include <stdio.h>
 
+/* This macro generate a different code if x is double, int... */
 #define PRINT(x) _Generic((x), double: printf("double -> %f\n", x), \
                               default: printf("Unsupported type.\n"), \
                               int: printf("int -> %d\n", x), \
                               char *: printf("string -> %s\n", x))
+
+struct a{
+    int null;
+};
+struct b{
+    int null;
+};
+
+/* This macro can check is a struct as an attribute null == 1, whatever the struct. */
+#define IS_NULL(x) x.null == 1
 
 int main()
 {
@@ -12,6 +23,10 @@ int main()
     PRINT(a);
     PRINT("gogo");
     PRINT((char)'A');
+    struct a A = {1};
+    struct b B = {0};
+    PRINT(IS_NULL(A));
+    PRINT(IS_NULL(B));
 
     return 0;
 }

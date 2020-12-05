8ece9773201e8e7b9ad97cd6d7892518979147dc
François NOUAILLE DEGORCE <francois.nouaille-degorce@outscale.com>
2020-12-05 19:01:58 +0100

    wip

--- macros/list.c
+++ macros/list.c
@@ -3,30 +3,33 @@
 #include <stdlib.h>
 
 #define LIST_DEFINE(TYPE, name) \
-    typedef struct                 \
+    typedef struct              \
     {                           \
         int size;               \
         TYPE *list;             \
     } name
 
-#define LIST_INIT(name) \
-    {                  \
-        .size = 0,     \
-        .list = NULL,  \
-    }
-#define LIST_NEW(name, l) \
-    l = malloc(sizeof(name));\
-    l->size = 0; \
+#define LIST_NEW(name, l)     \
+    l = malloc(sizeof(name)); \
+    l->size = 0;              \
     l->list = NULL
-    
-#define LIST_ADD(list, element) \
 
+#define LIST_ADD(list, element)
+
+#define LIST_FREE(l) \
+    if(l->size != 0)\
+    {\
+     \
+\
+    } \
+    free(l)
 
 LIST_DEFINE(int, maliste);
 
 int main()
 {
-    maliste * ml;
+    maliste *ml;
     LIST_NEW(maliste, ml);
+    LIST_FREE(ml);
     return 0;
 }

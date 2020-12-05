4f85724132e78274282adc1d46da46a3a4f0f46e
François NOUAILLE DEGORCE <francois.nouaille-degorce@outscale.com>
2020-12-05 18:59:01 +0100

    wip

--- macros/list.c
+++ macros/list.c
@@ -1,23 +1,32 @@
-#!/ usr / local / bin / tcc - run
+#!/usr/local/bin/tcc -run
 #include <stdio.h>
+#include <stdlib.h>
 
 #define LIST_DEFINE(TYPE, name) \
-    struct name                 \
+    typedef struct                 \
     {                           \
         int size;               \
         TYPE *list;             \
-    }
+    } name
 
-#define LIST_NEW(name) \
+#define LIST_INIT(name) \
     {                  \
         .size = 0,     \
         .list = NULL,  \
     }
+#define LIST_NEW(name, l) \
+    l = malloc(sizeof(name));\
+    l->size = 0; \
+    l->list = NULL
+    
+#define LIST_ADD(list, element) \
+
 
 LIST_DEFINE(int, maliste);
 
 int main()
 {
-    struct maliste ml = LIST_NEW(maliste);
+    maliste * ml;
+    LIST_NEW(maliste, ml);
     return 0;
 }

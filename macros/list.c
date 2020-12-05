34a7feda0237e1039584568b3cd300895102fdcd
François NOUAILLE DEGORCE <francois.nouaille-degorce@outscale.com>
2020-12-05 19:32:39 +0100

    wip

--- macros/list.c
+++ macros/list.c
@@ -6,6 +6,7 @@
     typedef struct              \
     {                           \
         int size;               \
+        int elm_size;           \
         TYPE *list;             \
     } name
 
@@ -14,7 +15,17 @@
     l->size = 0;              \
     l->list = NULL
 
-#define LIST_ADD(list, element)
+#define LIST_ADD(l, element)                                   \
+    l->size += 1;                                              \
+    if (l->size == 1)                                          \
+    {                                                          \
+        l->list = malloc(sizeof(element));                     \
+    }                                                          \
+    else                                                       \
+    {                                                          \
+        l->list = realloc(l->list, sizeof(element) * l->size); \
+    }                                                          \
+    l->list[l->size - 1] = element
 
 #define LIST_FREE(l)   \
     if (l->size != 0)  \
@@ -29,6 +40,7 @@ int main()
 {
     maliste *ml;
     LIST_NEW(maliste, ml);
+    LIST_ADD(ml, 1);
     LIST_FREE(ml);
     return 0;
 }

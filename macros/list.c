d972d0fe482e8f8960a07d66402f9230ffc675fc
François NOUAILLE DEGORCE <nouaille.degorce.francois@gmail.com>
2020-12-06 20:27:42 +0100

    wip: lists are not allocated via malloc.

--- macros/list.c
+++ macros/list.c
@@ -4,13 +4,10 @@
 
 #define PRINT_DELIMITER() printf("-------------\n");
 
-LIST_DEFINE(int, maliste);
-LIST_DEFINE(char *, maliste_string);
-
 int main()
 {
-    maliste *ml;
-    LIST_NEW(maliste, ml);
+    struct test{int i;} montest = {1};
+    LIST_NEW(ml, int);
     LIST_ADD(ml, 1);
     LIST_ADD(ml, 1);
     LIST_ADD(ml, 3);
@@ -21,37 +18,8 @@ int main()
     }
     PRINT_DELIMITER();
     LIST_FREE(ml);
-    LIST_NEW(maliste, ml);
-    LIST_ADD(ml, 1);
-    LIST_ADD(ml, 4);
-    LIST_ADD(ml, 3);
-    LIST_ADD(ml, 13);
-    LIST_FOR(ml, i)
-    {
-        printf("%d\n", ml->list[i]);
-    }
-    PRINT_DELIMITER()
-    LIST_POP_INDEX(ml, 1);
-    LIST_FOREACH(ml, elem)
-    {
-        printf("%d\n", *elem);
-    }
-    PRINT_DELIMITER()
-    LIST_FREE(ml);
-    LIST_NEW(maliste, ml);
-    LIST_ADD(ml, 1);
-    LIST_POP_INDEX(ml, 0);
-    LIST_ADD(ml, 2);
-    LIST_FOR(ml, i)
-    {
-        printf("%d\n", ml->list[i]);
-    }
-    PRINT_DELIMITER();
-    LIST_FREE(ml);
-    printf("%d\n", LIST_COUNT);
-    PRINT_DELIMITER();
-    maliste_string * mls;
-    LIST_NEW(maliste_string, mls);
+
+    LIST_NEW(mls, char *);
     LIST_SET_FREE_FUNC(mls, free);
     char * s1 = malloc(sizeof(char) * 3);
     char * s2 = malloc(sizeof(char) * 3);
--- macros/list.h
+++ macros/list.h
@@ -18,91 +18,71 @@
 /* How many lists are active and need to be freed. */
 int LIST_COUNT = 0;
 
-/* 
- * Generate the header of the list.
- *
- * type: the type of stored elements.
- * name: the name of the declared struct.
- */
-
-#define LIST_DEFINE(type, name) \
-    typedef struct              \
-    {                           \
-        int size;               \
-        int elm_size;           \
-        type *list;             \
-        int (*free)(type);      \
-    } name
-
-/*
- * Create a new instance of a list.
- *
- * name: the name of the struct to intantiate (given at the LIST_DEFINE).
- * l: the  pointer of the list (type: name*).
- */
-
-#define LIST_NEW(name, l)     \
-    LIST_COUNT++;             \
-    l = malloc(sizeof(name)); \
-    l->size = 0;              \
-    l->list = NULL;           \
-    l->free = NULL
+#define LIST_NEW(name, type)            \
+    struct name##_LIST                  \
+    {                                   \
+        int size;                       \
+        type *list;                     \
+        int (*free)(type);              \
+    } name;                             \
+    name.size = 0;                     \
+    name.list = NULL;                  \
+    name.free = NULL
 
 #define LIST_ADD(l, element)                                                    \
-    l->size += 1;                                                               \
-    if (l->size == 1)                                                           \
+    l.size += 1;                                                               \
+    if (l.size == 1)                                                           \
     {                                                                           \
-        l->list = malloc(sizeof(element));                                      \
+        l.list = malloc(sizeof(element));                                      \
     }                                                                           \
     else                                                                        \
     {                                                                           \
-        l->list = (typeof(l->list))realloc(l->list, sizeof(element) * l->size); \
+        l.list = (typeof(l.list))realloc(l.list, sizeof(element) * l.size); \
     }                                                                           \
-    l->list[l->size - 1] = element;                                             \
-    l->size - 1
+    l.list[l.size - 1] = element;                                             \
+    l.size - 1
 
 #define LIST_FOREACH(l, elem) \
-    for (typeof(l->list) elem = l->list; elem < l->list + l->size; elem++)
+    for (typeof(l.list) elem = l.list; elem < l.list + l.size; elem++)
 
 #define LIST_FREE(l)              \
     LIST_COUNT--;                 \
-    if (l->size != 0)             \
+    if (l.size != 0)             \
     {                             \
-        if (l->free != NULL)      \
+        if (l.free != NULL)      \
         {                         \
             LIST_FOREACH(l, elem) \
             {                     \
-                l->free(*elem);   \
+                l.free(*elem);   \
             }                     \
         }                         \
-        free(l->list);            \
+        free(l.list);            \
     }                             \
-    l->size = 0;                  \
-    free(l)
+    l.size = 0;
 
 #define LIST_FOR(l, index) \
-    for (int index = 0; index < l->size; index++)
+    for (int index = 0; index < l.size; index++)
 
 #define LIST_POP_INDEX(l, index)                                                   \
-    if (l->free != NULL)                                                           \
+    if (l.free != NULL)                                                           \
     {                                                                              \
-        l->free(l->list[index]);                                                   \
+        l.free(l.list[index]);                                                   \
     }                                                                              \
-    for (int i = index; i < l->size - 1; i++)                                      \
+    for (int i = index; i < l.size - 1; i++)                                      \
     {                                                                              \
-        l->list[i] = l->list[i + 1];                                               \
+        l.list[i] = l.list[i + 1];                                               \
     }                                                                              \
-    l->size--;                                                                     \
-    if (l->size > 0)                                                               \
+    l.size--;                                                                     \
+    if (l.size > 0)                                                               \
     {                                                                              \
-        l->list = (typeof(l->list))realloc(l->list, sizeof(l->list[0]) * l->size); \
+        l.list = (typeof(l.list))realloc(l.list, sizeof(l.list[0]) * l.size); \
     }                                                                              \
     else                                                                           \
     {                                                                              \
-        free(l->list);                                                             \
+        free(l.list);                                                             \
     }
 
 #define LIST_SET_FREE_FUNC(list, func) \
-    list->free = func
+    list.free = func
 
 #endif
